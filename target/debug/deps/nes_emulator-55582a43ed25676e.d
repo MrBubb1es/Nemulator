d5e30c437da22824a5a9d00466030c66ea379834
MrBubb1es <lance04preston@gmail.com>
2024-05-11 17:12:50 -0500

    Fixed logan's nonsense

--- target/debug/deps/nes_emulator-55582a43ed25676e.d
+++ target/debug/deps/nes_emulator-55582a43ed25676e.d
@@ -1,8 +1,12 @@
-/home/lance/projects/nes_emulator/target/debug/deps/nes_emulator-55582a43ed25676e: src/main.rs src/cartridge/mod.rs src/cartridge/cartridge.rs src/cartridge/reader.rs
+/home/lance/projects/nes_emulator/target/debug/deps/nes_emulator-55582a43ed25676e: src/main.rs src/cartridge/mod.rs src/cartridge/cartridge.rs src/cartridge/reader.rs src/system/mod.rs src/system/mem.rs src/system/bus.rs src/system/cpu.rs
 
-/home/lance/projects/nes_emulator/target/debug/deps/nes_emulator-55582a43ed25676e.d: src/main.rs src/cartridge/mod.rs src/cartridge/cartridge.rs src/cartridge/reader.rs
+/home/lance/projects/nes_emulator/target/debug/deps/nes_emulator-55582a43ed25676e.d: src/main.rs src/cartridge/mod.rs src/cartridge/cartridge.rs src/cartridge/reader.rs src/system/mod.rs src/system/mem.rs src/system/bus.rs src/system/cpu.rs
 
 src/main.rs:
 src/cartridge/mod.rs:
 src/cartridge/cartridge.rs:
 src/cartridge/reader.rs:
+src/system/mod.rs:
+src/system/mem.rs:
+src/system/bus.rs:
+src/system/cpu.rs:
